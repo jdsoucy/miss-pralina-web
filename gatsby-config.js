76397525026231598697a928892f294952e3e15f
Jérôme D.Soucy <jerome.duguay-soucy@workjam.com>
2021-06-15 01:31:35 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "Miss Pralina",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+  ],
+};
